01da8b97b0a11632cbc2362b75a903a73749c9ec
pasta0119 <spaghetti0119@gmail.com>
2023-02-24 21:51:17 +0900

    20230224

---
+++ practice.java
@@ -0,0 +1,13 @@
+public class practice
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello, ");
+		System.out.println("JAVA!");
+		System.out.print("Hello, JAVA!\n");
+		System.out.println( 2 + 1 );
+		System.out.println("X : " + 1);
+		System.out.println("Y : " + 2);
+		System.out.println("Z = X + Y : " + (1 + 2));
+	}
+}
